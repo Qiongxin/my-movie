0ddc4d2a8ebd2e9c9661c54d3a5e1870bd0e8398
Qiongxin Zhang <qiongxin@ualberta.ca>
2022-12-07 22:18:51 -0800

    rows

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import axios from './axios'
+import React, { useEffect, useState } from 'react'
 import "./Banner.scss"
+import requests from './Request'
 
 function Banner() {
 
+  const[movie, setMovie] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals)
+
+      setMovie(
+        request?.data.results[Math.floor(Math.random() * request.data.results.length - 1)]
+      )
+    }
+  
+    fetchData()
+  }, [])
+
   const overflow = (text, n) => {
-    return text.length > n ? text.substring(0, n-1) + "..." : text
+    return text?.length > n ? text.substring(0, n-1) + "..." : text
   }
 
   return (
-    <div className="banner">
+    <div className="banner" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`}}>
       <div className="info-wrapper">
-        <h1>movie title</h1>
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
         <div className="play-buttons">
           <button type="button" className="play-button">Play</button>
           <button type="button" className="play-button">My List</button>
         </div>
-        <p>{overflow('this is a subscription for movies', 150)}</p>
+        <p>{overflow(movie?.overview, 120)}</p>
       </div>
+      {/* little trick to show shadow in the bottom of banner */}
       <div className="shadow"></div>
     </div>
   )
--- src/components/Rows.js
+++ src/components/Rows.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import axios from './axios'
+import React, { useEffect, useState } from 'react'
+import "./Rows.scss"
+function Rows({title, fetchUrl, isLargeRow=false}) {
+  const [movies, setMovies] = useState([])
 
-function Rows() {
-  return (
-    <div>
+  //get the movies list from database and set to movies
+  // add fetchUrl as dependency, runs when fetchUrl changes
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl)
+
+      setMovies(
+        request.data.results
+      )
+    }
     
+    fetchData()
+  }, [fetchUrl])
+  
+  return (
+    <div className="Rows">
+      <h1>{title}</h1>
+      
+      {/* use isLargeRow to decide add extra className for poster or not */}
+      <div className="posters">
+
+        {/* if isLargeRow set to true, render poster image, if it's not, render backdrop path
+            in order to avoid dead link, only render the poster or backdrop if it exist */}
+        {movies.map(movie => 
+          ((movie.backdrop_path && !isLargeRow) || (movie.poster_path && isLargeRow)) &&
+          (<img className={`poster ${isLargeRow && "poster_large"}`} key={movie.id} src=
+          {`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt="movie" />)
+        )}
+      </div>
     </div>
   )
 }
